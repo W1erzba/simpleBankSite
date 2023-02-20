00545bb6345679a936288c425c6f522a8015080f
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-02-20 18:02:37 +0100

    stickyNav

--- script.js
+++ script.js
@@ -112,7 +112,22 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
-
 // Passing 'argument' into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky nav bar with IntersectionObserver
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
+});
+
+headerObserver.observe(header);
